145633eb21263d48205f1a17530dd006fd822830
Francisco Ferrández <fferrandez@iti.es>
2021-10-16 12:18:04 +0200

    User Swagger configuration

---
+++ api/src/Service/User/UserRegisterService.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service\User;
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\Request;
+
+class UserRegisterService
+{
+    public function __construct()
+    {
+
+    }
+
+    public function create(Request $request): User
+    {
+
+    }
+}
